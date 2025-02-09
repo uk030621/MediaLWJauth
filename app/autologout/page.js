c3e0cba157f7eb50f3d705cfa72ee3b6a29af969
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-02-09 11:59:44 +0000

    Give user option to stay logged in.

--- app/autologout/page.js
+++ app/autologout/page.js
@@ -3,7 +3,7 @@
 import { useEffect } from "react";
 import { signOut } from "next-auth/react";
 
-const INACTIVITY_LIMIT = 10 * 60 * 1000; // Move outside the component
+const INACTIVITY_LIMIT = 20 * 60 * 1000; // 10 minutes
 
 const AutoLogout = () => {
   useEffect(() => {
@@ -12,12 +12,19 @@ const AutoLogout = () => {
     const resetTimer = () => {
       clearTimeout(timeout);
       timeout = setTimeout(() => {
-        alert("You will be logged out soon due to inactivity.");
-        setTimeout(() => signOut(), 30000); // Logout after 30 seconds
+        const stayLoggedIn = confirm(
+          "You will be logged out soon due to inactivity. Do you want to stay logged in?"
+        );
+
+        if (stayLoggedIn) {
+          resetTimer(); // Restart the timer if the user chooses to stay
+        } else {
+          setTimeout(() => signOut(), 30000); // Logout after 30 seconds
+        }
       }, INACTIVITY_LIMIT - 30000);
     };
-    const events = ["mousemove", "keydown", "scroll", "click"];
 
+    const events = ["mousemove", "keydown", "scroll", "click"];
     events.forEach((event) => window.addEventListener(event, resetTimer));
 
     resetTimer();
@@ -26,7 +33,7 @@ const AutoLogout = () => {
       clearTimeout(timeout);
       events.forEach((event) => window.removeEventListener(event, resetTimer));
     };
-  }, []); // No need for dependencies now
+  }, []);
 
   return null;
 };
