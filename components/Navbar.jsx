394675880fdcb2b3b485019c9de276084192276f
uk030621 <123797901+uk030621@users.noreply.github.com>
2025-02-09 09:51:19 +0000

    Added ability to add more links to Navbar and scroll horizontally to view.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,43 +16,46 @@ const Navbar = () => {
   return (
     <header>
       <nav className="navbar">
-        <ul className="nav-links flex justify-center items-center">
-          <li>
-            <Link href="/" className="text-xs md:text-lg lg:text-xl">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link href="/youtube" className="text-xs md:text-lg lg:text-xl">
-              YouTube
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/customsearch"
-              className="text-xs md:text-lg lg:text-xl"
-            >
-              URL
-            </Link>
-          </li>
-          <li>
-            <Link href="/enhanced" className="text-xs md:text-lg lg:text-xl">
-              Library
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="#"
-              onClick={(e) => {
-                e.preventDefault(); // Prevent default link behavior
-                signOut(); // Call signOut function
-              }}
-              className="text-xs md:text-lg lg:text-xl cursor-pointer"
-            >
-              Exit
-            </Link>
-          </li>
-        </ul>
+        {/* 🔹 Scrollable container for horizontal navigation */}
+        <div className="nav-links-container">
+          <ul className="nav-links flex justify-center items-center">
+            <li>
+              <Link href="/" className="text-xs md:text-lg lg:text-xl">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/youtube" className="text-xs md:text-lg lg:text-xl">
+                YouTube
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/customsearch"
+                className="text-xs md:text-lg lg:text-xl"
+              >
+                URL
+              </Link>
+            </li>
+            <li>
+              <Link href="/enhanced" className="text-xs md:text-lg lg:text-xl">
+                Library
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault(); // Prevent default link behavior
+                  signOut(); // Call signOut function
+                }}
+                className="text-xs md:text-lg lg:text-xl cursor-pointer"
+              >
+                Exit
+              </Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
